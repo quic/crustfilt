ad402e9b58d36ce7836b65f457b6f12e12f9c7a8
Sudharsan Veeravalli <quic_svs@quicinc.com>
2024-12-05 15:26:04 +0530

    Fix for exclusive range pattern error
    Get the following error while trying to compile:
    
       Compiling crustfilt v0.1.0 (/local/mnt/workspace/svs/crustfilt)
    error[E0658]: exclusive range pattern syntax is experimental
      --> src/decoder.rs:37:13
       |
    37 |             6..15 => write!(f, "CmPush {{ra, s0-s{}}},", self.urlist - 5),
       |             ^^^^^
       |
    
    Using: rustc 1.75.0 (82e1608df 2023-12-21) (built from a source tarball)
    
    Also removed an unused variable.
    
    Signed-off-by: Sudharsan Veeravalli <quic_svs@quicinc.com>

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,6 @@ fn parse_line(s: &String) -> Result<String, ()> {
 }
 
 fn main() {
-    let cli = Cli::parse();
-
     for line in std::io::stdin().lines() {
         if let Ok(data) = line {
             if let Ok(dline) = parse_line(&data) {
